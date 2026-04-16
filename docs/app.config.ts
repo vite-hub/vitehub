4493d75a75e6af87f973c5bb2bd04e501fb2eb55
onmax <maximogarciamtnez@gmail.com>
2026-04-16 06:55:29 +0200

    refactor(docs): use docus defaults for sticky offset

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -33,16 +33,6 @@ export default defineAppConfig({
         linkTrailing: "hidden",
       },
     },
-    pageAside: {
-      slots: {
-        root: "hidden overflow-y-auto lg:block lg:max-h-[calc(100vh-var(--vitehub-docs-sticky-offset))] lg:sticky lg:top-(--vitehub-docs-sticky-offset) py-8 lg:ps-4 lg:-ms-4 lg:pe-6.5",
-      },
-    },
-    contentToc: {
-      slots: {
-        root: "sticky top-(--vitehub-docs-sticky-offset) z-10 bg-default/75 lg:bg-[initial] backdrop-blur -mx-4 px-4 sm:px-6 sm:-mx-6 lg:ms-0 overflow-y-auto max-h-[calc(100vh-var(--vitehub-docs-sticky-offset))]",
-      },
-    },
     pageLinks: {
       slots: {
         linkLeadingIcon: "size-4",
