1ce4b39cff13f84741f34fc5a52d34d743365469
onmax <maximogarciamtnez@gmail.com>
2026-04-16 08:23:52 +0200

    remove dead type shims

--- docs/types/query-collection.global.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Collections, CollectionQueryBuilder } from "@nuxt/content";
-
-declare global {
-  const queryCollection: <T extends keyof Collections>(collection: T) => CollectionQueryBuilder<Collections[T]>;
-}
-
-export {};
