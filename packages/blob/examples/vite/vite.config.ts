07dd2e96527634b27ac3df3ff1b016c080f7e063
onmax <maximogarciamtnez@gmail.com>
2026-04-19 09:16:33 +0200

    docs(blob): bootstrap vite example via virtual config

--- packages/blob/examples/vite/vite.config.ts
+++ packages/blob/examples/vite/vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite"
 import { hubBlob } from "@vitehub/blob/vite"
 
 export default defineConfig({
+  blob: { driver: "vercel-blob" },
   plugins: [hubBlob()],
-  server: {
-    port: 5173,
-  },
+  server: { port: 5173 },
 })
