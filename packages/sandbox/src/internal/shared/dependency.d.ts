ccbac7897fdb718b5448eaacbd1d62c99f4c16f8
onmax <maximogarciamtnez@gmail.com>
2026-04-26 16:12:36 +0200

    chore(sandbox): drop orphan d.ts + share isPlainObject

--- packages/sandbox/src/internal/shared/dependency.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function hasInstalledDependency(deps: Record<string, string>, dependency: string, options?: {
-    paths?: string[];
-}): boolean;
--- packages/sandbox/src/internal/shared/feature-options.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function isPlainObject(value: unknown): value is Record<string, unknown>;
-export declare function cloneFeatureOptions<T extends object>(feature: string, options: T): T;
-export declare function normalizeFeatureOptions<T extends object>(feature: string, options: T | false | undefined): T | undefined;
